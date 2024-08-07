d6facde2c4edfbe967097e384a05731a9ee1aed4
ymndnss <yamnadenise05@gmail.com>
2024-08-07 11:00:19 -0300

    creamos metodo 'obtenerproducto' para visualizar productos actuales en la base de datos; creamos un modal para agregar nuevos productos mediante un formulario; haciendo una directiva; creamos una funcion que tome los valores ingresados por el usuario

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -6,17 +6,44 @@ import { CrudService } from '../../services/crud.service';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent {
-coleccionProductos:Producto[]=[];
-producto=new FormGroup({
-  nombre: new FormControl('validators.required'),
-  precio: new FormControl (0,Validators.required),
-  descripcion: new FormControl ('',Validators.required),
-  categoria: new FormControl ('',Validators.required), 
-  imagen: new FormControl ('',Validators.required),
-  alt: new FormControl ('', Validators.required)
-})
-constructor(public servicioCrud: CrudService){}
+  coleccionProductos: Producto[] = [];
+  producto = new FormGroup({
+    nombre: new FormControl('validators.required'),
+    precio: new FormControl(0, Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required),
+    alt: new FormControl('', Validators.required),
+  });
+  constructor(public servicioCrud: CrudService) {}
+  ngOnInit(): void {
+    //subscribe=notifica constantemente los cambios acutalesw del sistema
+    this.servicioCrud.obtenerProducto().subscribe((producto) => {
+      this.coleccionProductos = producto;
+    });
+  }
+  async agregarProducto() {
+    if (this.producto.valid) {
+      let nuevoProducto: Producto = {
+        idProducto: '',
+        nombre: this.producto.value.nombre!,
+        descripcion: this.producto.value.descripcion!,
+        precio: this.producto.value.precio!,
+        categoria: this.producto.value.categoria!,
+        imagen: this.producto.value.imagen!,
+        alt: this.producto.value.alt!,
+      };
+      await this.servicioCrud
+        .crearProducto(nuevoProducto)
+        .then((roducto) => {
+          alert('ha agregado un producto con exito');
+        })
+        .catch((error) => {
+          alert('hubo un problema con al agregar un producto');
+        });
+    }
+  }
 }
--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -1,43 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-  export class CrudService {
-    // Definimos colección para los productos de la web del tipo Producto
-    private productosCollection: AngularFirestoreCollection<Producto>
-  
-    constructor(private database: AngularFirestore) {
-      // Referenciamos colección productos y será subida como "producto" a Firebase
-      this.productosCollection = database.collection('producto');
-    }
-  
-    // CREAR productos
-    crearProducto(producto: Producto){
-      return new Promise(async (resolve, reject) => {
-        try{
-          // Creamos número identificativo para el producto en la base de datos
-          const idProducto = this.database.createId();
-  
-          // Asignamos ID creado al atributo idProducto de la interfaz "Producto"
-          producto.idProducto = idProducto;
-  
-          const resultado = await this.productosCollection.doc(idProducto).set(producto);
-  
-          resolve(resultado);
-        }catch(error){
-          reject(error);
-        }
-      })
-    }
-  
-    // OBTENER productos
+export class CrudService {
+  // Definimos colección para los productos de la web del tipo Producto
+  private productosCollection: AngularFirestoreCollection<Producto>;
+
+  constructor(private database: AngularFirestore) {
+    // Referenciamos colección productos y será subida como "producto" a Firebase
+    this.productosCollection = database.collection('producto');
+  }
+
+  // CREAR productos
+  crearProducto(producto: Producto) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        // Creamos número identificativo para el producto en la base de datos
+        const idProducto = this.database.createId();
+
+        // Asignamos ID creado al atributo idProducto de la interfaz "Producto"
+        producto.idProducto = idProducto;
+
+        const resultado = await this.productosCollection
+          .doc(idProducto)
+          .set(producto);
+
+        resolve(resultado);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  // OBTENER productos
+  obtenerProducto() {
+    return this.productosCollection
+    //snapshotchanges= toma una captura del estado de los datos
+    //pipe= funciona como una tuberia que retorna el nuevo arreglo
+    //map= recorre esa nueva informacion 
+    //a= resguarada la nueva informacion y la envia
+      .snapshotChanges().pipe(map(Action => Action.map(a => a.payload.doc.data())));
     // EDITAR productos
     // ELIMINAR productos
   }
-
-
-
-
+}
